be25724fd9cd06e319b0237690ab4da7f894c834
Shan <128463863+shanbhat2005@users.noreply.github.com>
2026-01-02 20:35:18 +0000

    practice file created

--- learning/index.js
+++ learning/index.js
@@ -7,3 +7,26 @@ baadmerun(function (){
 })
 
 // callback is when a function is passed inside another function 
+
+
+function profileLekarAao(username,callback){
+setTimeout(function(){
+console.log(`profile name is ${username}`)
+callback({rollno:101,name:"shan",year:3})
+},1000)
+}
+
+function resultDikhao(rollno,cb){
+    setTimeout(() => {
+        console.log(`your rollno is ${rollno}`)
+        cb({result:"pass"});
+    }, 2000);
+
+}
+profileLekarAao("shan",function(cbData){
+    // console.log(cbData)
+    resultDikhao(cbData.rollno,function(result){
+console.log(result);
+    })
+    
+})
