a2809cafa45f721dbfdd2ba8ab6c250e27443116
Shan <128463863+shanbhat2005@users.noreply.github.com>
2026-01-03 13:06:06 +0000

    async await started

--- learning/index.js
+++ learning/index.js
@@ -33,23 +33,45 @@
 
 // promises are used when our data is only in 2 forms either it is resolved or it is rejected
 
-let pr = new Promise(function (res, rej) {
+// let pr = new Promise(function (res, rej) {
+//     setTimeout(() => {
+//         let err = false;
+
+//         if (err == false) {
+//             res('success...')
+//         }
+//         else {
+//             rej('failed...')
+//         }
+//     }, 2000);
+// })
+
+// pr.then(function (data) {
+//     console.log(data)
+// })
+
+// pr.catch(function (data) {
+//     console.log(data)
+// })
+
+// async/await
+
+let pr = new Promise(function(res,rej){
     setTimeout(() => {
-        let err = false;
+        if((Math.floor(Math.random)*10)<5){
+        res("resolved...")
+    }
+    else rej('rejected...');
+    }, 1000);
+});
 
-        if (err == false) {
-            res('success...')
-        }
-        else {
-            rej('failed...')
-        }
-    }, 2000);
-})
+async function shan(){
+    try{
+        let ans = await pr;
+        console.log(ans)
+    }catch(err){
+console.log(err);
+    }
+}
 
-pr.then(function (data) {
-    console.log(data)
-})
-
-pr.catch(function (data) {
-    console.log(data)
-})
+shan()
